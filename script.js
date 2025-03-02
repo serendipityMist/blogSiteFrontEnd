f37aacdcc98d969549818e8ec8405de1fcb207b5
serendipityMist <serendipityMist@gmail.com>
2025-03-02 19:23:54 +0545

    initial site

---
+++ script.js
@@ -0,0 +1,83 @@
+const message = document.querySelector("#message");
+const submit = document.querySelector("#submit");
+const postContainer = document.querySelector("#messages");
+const messageTitle = document.querySelector("#title");
+let wordCount = document.querySelector("#wordCount");
+let postMessage = [];
+let comments = [];
+let postID = 0;
+
+message.addEventListener("input", () => {
+  console.log(`The word count is ${message.value.split(/\s+/).length}`);
+  wordCount.textContent = `Count: ${message.value.split(/\s+/).length}`;
+});
+
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (message.value.trim() === "" || messageTitle.value.trim() === "") {
+    alert("Please enter some message before proceeding");
+    return;
+  }
+  if (message.value.split(/\s+/).length < 30) {
+    alert("The word cound must be up to 30.");
+    return;
+  }
+
+  postMessage.push({
+    message: message.value,
+    postID: postID,
+    title: messageTitle.value,
+  });
+
+  messageTitle.value = "";
+  message.value = "";
+  wordCount.textContent = "";
+  createPostBox(postMessage);
+  console.log(postMessage);
+});
+
+function createPostBox(postMessage) {
+  postContainer.innerHTML = "";
+  postMessage.map((post, index) => {
+    post.postID = index + 1;
+    posts(post);
+  });
+}
+function posts(post) {
+  const postDiv = document.createElement("div");
+  postDiv.classList.add("posts", `post-${post.postID}`);
+
+  const topSection = document.createElement("div");
+  topSection.classList.add("titleDelete");
+
+  const postTitle = document.createElement("h1");
+  postTitle.textContent = post.title;
+
+  const postText = document.createElement("p");
+  postText.textContent = post.message;
+
+  const delBtn = document.createElement("button");
+  delBtn.textContent = "Delete";
+
+  postDiv.appendChild(topSection);
+  topSection.appendChild(postTitle);
+  topSection.appendChild(delBtn);
+  postDiv.appendChild(postText);
+
+  //TODO: ADD COMMENT SECTION
+
+  delBtn.addEventListener("click", () => {
+    console.log(`The post ID is ${post.postID}`);
+    console.log(`Post Message Value :`, [postMessage]);
+    postDiv.remove();
+    delPost(post.postID);
+  });
+  postContainer.appendChild(postDiv);
+}
+
+function delPost(id) {
+  postMessage = postMessage.filter((element) => element.postID != id);
+
+  console.log(postMessage);
+}
